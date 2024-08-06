b3e66268e1c3dc9286f6a4700d65e37867a24d6b
ABID ALI <81401679+Abidali665@users.noreply.github.com>
2024-08-06 08:13:29 +0100

    add example folder

---
+++ example html/pritice.js
@@ -0,0 +1,18 @@
+var student=huzifa;
+var age=12;
+var married=false;
+var num1=20;
+var num2=30;
+var sum=num1+num2;
+console.log("product", num1*num2);
+age=age+5;
+age *=10;
+var vehicle = {
+    model: "2023",
+    company: "Toyota",
+    engine: 1600,
+    color: "black",
+    name: "Audi",
+    start: function () {
+        console.log("Car Engine is started...")
+    }
